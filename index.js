bebdee4028ae4629b915c93bd0b5cb499c7eab65
ryan atienza <ratienza@ucsd.edu>
2026-04-24 19:35:05 -0700

    minor format v33

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
-const githubData = await fetchGitHubData('giorgianicolaou');
+const githubData = await fetchGithubData('popokeyry');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
+    `;
+}
 
 renderProjects(latestProjects, projectsContainer, 'h2');
