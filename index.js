98874366568bea51cb0fb218736a2817ccdf3bb4
ryan atienza <ratienza@ucsd.edu>
2026-04-24 19:26:55 -0700

    step 3 v30

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
+const githubData = await fetchGitHubData('giorgianicolaou');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
